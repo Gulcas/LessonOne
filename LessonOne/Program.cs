b0bcaf41e32319fc0e4d07200d2eb08ea45d17b8
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-02-12 19:14:54 +0100

    Begining

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -10,6 +10,20 @@ namespace LessonOne
     {
         static void Main(string[] args)
         {
+            int firstValue = 10; //variable int
+            int secondValue;
+            secondValue = firstValue;  //przekazanie przez wartość z jednej do drugiej variable
+            Console.WriteLine(firstValue);
+            Console.WriteLine(firstValue + secondValue);
+
+            Console.ReadKey();
+
+            string firstString = "pierwyj";
+            string secondString = firstString + firstValue;
+            Console.WriteLine(firstString + secondString);
+
+            Console.ReadKey();
+
         }
     }
 }
