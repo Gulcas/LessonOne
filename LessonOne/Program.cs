8d7cc3cc99ddc01ab41e8367935db7c81a46be7c
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-02-12 19:50:46 +0100

    Kolejne zmiany

--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -18,12 +18,30 @@ namespace LessonOne
 
             Console.ReadKey();
 
-            string firstString = "pierwyj";
-            string secondString = firstString + firstValue;
-            Console.WriteLine(firstString + secondString);
+            string firstString = "romek";
+            string secondString = firstString + " 1001 " + firstValue;
+            Console.WriteLine(firstString);
+            Console.WriteLine(secondString);
 
             Console.ReadKey();
 
+            LessonOneClass2 obiektPierwszy = new LessonOneClass2(); //przywołanie obiektu utworzonego w drugiej klasie
+            LessonOneClass2 obiektDrugi = new LessonOneClass2();
+            obiektDrugi.NazwaKlasy = "jeden"; //określenie parametru utworzonego obiektu
+            obiektPierwszy.NazwaKlasy = "dwa";
+            
+            Console.WriteLine(obiektPierwszy.NazwaKlasy);
+            Console.WriteLine(obiektDrugi.NazwaKlasy); //wyrzucenie do konsoli przywołanego wcześniej obiektu
+            //którego wcześniej określiliśmy parametr
+            
+
+            obiektPierwszy.NazwaKlasy = "zmieniony";
+            obiektDrugi = obiektPierwszy; //przekazanie przez referencję
+
+            Console.WriteLine("obiekt pierwszy " + obiektPierwszy.NazwaKlasy); //wyrzenie na konsolę przekazanej wcześniej wartości
+            Console.WriteLine("obiekt drugi " + obiektDrugi.NazwaKlasy);
+
+            Console.ReadKey();
         }
     }
 }
