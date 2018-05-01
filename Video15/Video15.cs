a56bee9bddf0c743bf6e2dc39b83f3b1b473e90c
Rafa Granoszewski <rgranoszewski@outlook.com>
2018-05-01 10:41:56 +0200

    New Video

---
+++ Video15/Video15.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video15
+{
+    class Video15
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
