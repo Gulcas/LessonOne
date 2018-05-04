fd086ae74977c4c57fdb520f46150d37393bfbb0
Rafal Granoszewski <rgranoszewski@gmail.com>
2018-05-04 19:42:47 +0200

    New lessons

---
+++ Video17/Video17.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace Video17
+{
+    class Video17 //zrozumienie zakresów/ variables scope
+    {
+        private static string k = "sto milion siedem"; //zmienna dostępna w całej klasie Video17
+                                                       //private - metody prywatne mogą być wywołane jedynie przez metody w tej samej klasie
+                                                       //public - metody public mogą być wywołane w różnych klasach, są przez nie widoczne
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.DoSomething(); //przypisanie jedynej widocznej metody- publicznej
+
+            Console.WriteLine(myCar);
+            Console.ReadLine();
+        }
+
+    }
+    class Car
+    {
+        public void DoSomething() //utworzona metoda publiczna w klasie Car- jest widoczna i w klasie Video17
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod() //utworzona metoda prywatna która widoczna jest jedynie w klasie Car, a w klasie Video17 już nie
+        {
+            return "siema";
+        }
+    }
+}
---
+++ Video18/Video18.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace Video18
+{
+    class Video18
+    {
+        static void Main(string[] args)
+        {
+            string text = "To jest tekst";
+
+         
+            WebClient client = new WebClient(); //jeżeli wystąpił błąd bo używana klasa nie jest opisana na początku (using System...)
+            //to można użyć skrótu podpowiedzi CTRL+.
+            string replay = client.DownloadString("http://msdn.microsoft.com"); //za pomocą dwóch linijek kodu pobieram stronę internetową i zapisuję ją w pliku tekstowym jako html
+
+            Console.WriteLine(replay);
+            //System.IO.File.WriteAllText(@"C:\aaTest\teksior.txt", text); //pełen zapis
+            File.WriteAllText(@"C:\aaTest\teksior.txt", replay); //ale po użyciu na początku using System.IO możemy skrócić zapis
+            //wyszukując klas można użyć w wyszukiwarce- site:msdn.microsoft.com   wtedy wyszkiwane są strony tylko z microsoftu...
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
